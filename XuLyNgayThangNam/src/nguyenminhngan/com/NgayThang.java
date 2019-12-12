ab48a6130e6117f623ff1fa4376f671d0dd82694
NGAN <NGAN@ngmhngan>
2019-12-12 18:46:34 +0700

    Commit setting project

---
+++ XuLyNgayThangNam/src/nguyenminhngan/com/NgayThang.java
@@ -0,0 +1,10 @@
+package nguyenminhngan.com;
+
+public class NgayThang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nguyễn Minh Ngân");
+	}
+
+}
